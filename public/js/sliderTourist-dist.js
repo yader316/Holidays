2aeb691bcc0391b2ad49223f649542a41bb97589
Yader Mendoza <yadtais316@gmail.com>
2019-11-14 11:19:41 -0600

    se termino el slider del tourist country solo falta terminar el resposive del slider

---
+++ public/js/sliderTourist-dist.js
@@ -0,0 +1,1 @@
+var swiper=new Swiper(".swiper-container",{slidesPerView:1,spaceBetween:30,loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
